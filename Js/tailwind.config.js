839e9238e03f27c517ddde4ec2f67151c69d52d4
Ratul227 <ratulr5840@gmail.com>
2024-09-03 01:54:10 +0600

    first commit

---
+++ Js/tailwind.config.js
@@ -0,0 +1,41 @@
+tailwind.config = {
+    theme: {
+      extend: {
+        colors: {
+        menucollor:"#343d55",
+        bntcollor: "#5d86fe",
+        textcollor:"#d4d6db",
+        background:"#343d55",
+        background2:"#3f4963",
+        textcollor2:"#fbe3a1",
+        box:"#3f485e", 
+        box2:"#717888",
+         
+        
+        },
+        maxWidth: {
+            'headercontaner': '1350px',
+            'bannercontaner': '516px',
+            'bannercontaner2': '850px',
+         
+          },
+          fontFamily: {
+            'open': ['open sans'],
+            'inter': ['Inter'],
+            
+            },
+            backgroundImage: {
+              'banner': "url('./images/343d55.png')",
+              'banner2': "url('./images/GB.png')",
+              'contenar': "url('./images/contenar.png')",
+             
+            },
+            lineHeight: {
+              'bennetext': '100px',
+            }
+         
+      }
+    }
+  }
+
+  
